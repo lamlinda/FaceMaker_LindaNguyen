736270e7499ebd7975f0b4041dc4c181cf15b761
Linda <nguyenli23@up.edu>
2020-10-05 20:08:38 -0700

    Fixed "where T is a type-variable: T extends View declared in method <T>findViewById(int)" error by changing "Face" class to "FaceView"

--- app/src/main/java/com/example/facemaker_lindanguyen/FaceView.java
+++ app/src/main/java/com/example/facemaker_lindanguyen/FaceView.java
@@ -7,31 +7,57 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 
 import java.util.Random;
 
 public class FaceView extends SurfaceView {
     private int skinColor;
     private int eyeColor;
-    private int hairColor;
+
+    public int hairRed;
+    public int hairGreen;
+    public int hairBlue;
+
+    public int skinRed;
+    public int skinGreen;
+    public int skinBlue;
+
     private int hairStyle;
+
+    public int eyeRed;
+    public int eyeGreen;
+    public int eyeBlue;
+
+
+
+    public int hairC;
+    public int eyeC;
+    public int skinC;
 
 
     Paint skin = new Paint();
     Paint black = new Paint();
+    Paint eye = new Paint();
+    Paint hairColor = new Paint();
 
 
     public FaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
+        randomize();
 
-        skin.setColor(0xFFC755B5);
-        black.setColor(Color.BLACK);
 
         black.setStrokeWidth(8.0f);
         setBackgroundColor(Color.WHITE);  //better than black default
 
+        skinC = Color.rgb(skinRed, skinGreen, skinBlue);
+        hairC = Color.rgb(hairRed, hairGreen, hairBlue);
+        eyeC = Color.rgb(eyeRed, eyeGreen, eyeBlue);
+
     }
+
 
     public void randomize() {
 
@@ -41,52 +67,79 @@ public class FaceView extends SurfaceView {
          * */
         Random rand = new Random();
 
-        skinColor = rand.nextInt(255);
+        //randomize hair color
+        hairRed = rand.nextInt(255);
+        hairGreen = rand.nextInt(255);
+        hairBlue = rand.nextInt(255);
+
+        //randomize skin color
+        skinRed = rand.nextInt(255);
+        skinGreen = rand.nextInt(255);
+        skinBlue = rand.nextInt(255);
+
+        //randomize eyecolor
+        eyeRed = rand.nextInt(255);
+        eyeGreen = rand.nextInt(255);
+        eyeBlue = rand.nextInt(255);
+
         eyeColor = rand.nextInt(255);
-        hairColor = rand.nextInt(255);
         hairStyle = rand.nextInt(255);
+
+        //this.hairC = Color.rgb(hairRed, hairGreen, hairBlue);
+        //this.hairColor.setColor(hairC);
     }
 
 
     public void onDraw(Canvas canvas)
     {
 
-
+        hairColor.setColor(hairC);
+        skin.setColor(skinC);
+        eye.setColor(eyeC);
 
         canvas.drawCircle(500, 500, 250, skin);
-        drawFace(canvas);
-
+        //drawFace(canvas, black);
+        drawHair(canvas, hairStyle);
 
 
     }//onDraw
 
-    public void drawFace(Canvas canvas) {
-        canvas.drawCircle(400, 400, 50, black);
-        canvas.drawCircle(630, 400, 50, black);
-        canvas.drawOval(300, 0x258, 700, 550, black);
+    public void drawFace(Canvas canvas, Paint paint) {
+        canvas.drawCircle(400, 400, 50, paint);
+        canvas.drawCircle(630, 400, 50, paint);
+        canvas.drawOval(300, 0x258, 700, 550, paint);
     }
 
-    public void drawHairRectangleHair(Canvas canvas) {
-        //rectangle hair
-        canvas.drawRect(400, 230, 600, 330, black);
+    public void drawHair(Canvas canvas, int hair){
+        if(hair == 0){
+            //bowl hair
+            RectF rect = new RectF(300, 200, 700, 420);
+            canvas.drawArc(rect, 0, -180, true, hairColor);
+        }
+        else if(hair == 1){
+            //rectangle hair
+            canvas.drawRect(400, 230, 600, 330, hairColor);
+        }
+        else if(hair == 2){
+            //tassel hair
+            canvas.drawOval(300, 230, 700, 330, hairColor);
+            canvas.drawLine(350, 250, 350, 400, hairColor);
+            canvas.drawLine(400, 250, 400, 400, hairColor);
+            canvas.drawLine(450, 250, 450, 400, hairColor);
+            canvas.drawLine(500, 250, 500, 400, hairColor);
+            canvas.drawLine(550, 250, 550, 400, hairColor);
+            canvas.drawLine(600, 250, 600, 400, hairColor);
+        }
     }
 
-    public void DrawTassleHair(Canvas canvas){
-        //tassel hair
-        canvas.drawOval(300, 230, 700, 330, black);
-        canvas.drawLine(350, 250, 350, 400, black);
-        canvas.drawLine(400, 250, 400, 400, black);
-        canvas.drawLine(450, 250, 450, 400, black);
-        canvas.drawLine(500, 250, 500, 400, black);
-        canvas.drawLine(550, 250, 550, 400, black);
-        canvas.drawLine(600, 250, 600, 400, black);
+    public int getHairStyle(){
+        return this.hairStyle;
     }
 
-    public void drawbowlhair(Canvas canvas){
-        //bowl hair
-        RectF rect = new RectF(300, 200, 700, 420);
-        canvas.drawArc(rect, 0, -180, true, black);
+    public void setHairStyle(int hair){
+        this.hairStyle = hair;
     }
+
 }
 
 
--- app/src/main/java/com/example/facemaker_lindanguyen/MainActivity.java
+++ app/src/main/java/com/example/facemaker_lindanguyen/MainActivity.java
@@ -9,37 +9,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.camera2.params.Face;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     // array of strings in the spinner
     String[] hairStyles = {"Bowl", "Rectangle", "tassels"};
     private Spinner hairSpin;
+    private SeekBar redValue;
+    private SeekBar greenValue;
+    private SeekBar blueValue;
+
+    private RadioButton hairButton;
+    private RadioButton skinButton;
+    private RadioButton eyesButton;
+    FaceView faceV = null;
+    private ListenerClass listener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_main);
+
+        //I don't understand why but, when i name the face class just "Face" it gives me an error
+        this.faceV = findViewById(R.id.view);
+        listener = new ListenerClass(this.faceV);
 
         // code to populate spinner taken from class lectures
         hairSpin = findViewById(R.id.hairSpinner);
         ArrayAdapter<String> hairAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, this.hairStyles);
         hairSpin.setAdapter(hairAdapter);
+        hairSpin.setOnItemSelectedListener(this);
 
-        //Face faceV = findViewById(R.id.faceview);
 
-        //I don't understand why but, when i name the face class just "Face" it gives me an error
-        FaceView faceV = findViewById(R.id.test);
 
-        SeekBar redValue = findViewById(R.id.redSeekBar);
+
+        redValue = findViewById(R.id.redSeekBar);
+        redValue.setOnSeekBarChangeListener(listener);
+
+        greenValue = findViewById(R.id.greenSeekBar);
+       greenValue.setOnSeekBarChangeListener(listener);
+
+        blueValue = findViewById(R.id.blueSeekBar);
+        blueValue.setOnSeekBarChangeListener(listener);
+
+        hairButton = findViewById(R.id.hairRadio);
+        hairButton.setOnClickListener(listener);
+
+        skinButton = findViewById(R.id.skinRadio);
+        skinButton.setOnClickListener(listener);
+
+        eyesButton = findViewById(R.id.eyesRadio);
+        eyesButton.setOnClickListener(listener);
+
+        listener = new ListenerClass(faceV);
+        this.faceV = findViewById(R.id.view);
 
 
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        faceV.setHairStyle(position);
+        faceV.invalidate();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
